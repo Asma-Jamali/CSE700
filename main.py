eee0fd16f89b8b0ac888e1a7697613b511a2a95f
Uriel Garcilazo <garcilazo.uriel@gmail.com>
2025-04-08 16:41:34 -0400

    updated quick user guide

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ if __name__ == "__main__":
     data = pd.read_pickle(data_path)
     # kernel_ridge_regression(data, save_path="./results",kernel_name='gaussian', mol_property='Gap', representation='BOB', regularization=False)
 
-    from krr.plot_utils import plot_kernels
-    # Make sure to have tanimoto and dice kernels in the Results folder
-    plot_kernels(kernel_type="fingerprint_based", kernel_src_path=output_path, regularization=False)
-    # Make sure to have laplacian and gaussian kernels in the Results folder
-    plot_kernels(kernel_type="distance_based", kernel_src_path=output_path, regularization=False)
+    '''
+    Please uncomment the following lines to generate plots from the kernel ridge regression
+    analysis with different kernels and representations.
+    '''
+    # from krr.plot_utils import plot_kernels
+    
+    ## Make sure to have tanimoto and dice kernels in the Results folder
+    # plot_kernels(kernel_type="fingerprint_based", kernel_src_path=output_path, regularization=False)
+    
+    ## Make sure to have laplacian and gaussian kernels in the Results folder
+    # plot_kernels(kernel_type="distance_based", kernel_src_path=output_path, regularization=False)
